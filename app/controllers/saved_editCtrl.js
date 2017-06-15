84471bd7b6011c53ed0ef3a6a364613da0e0a900
Ed Sadler <ed_lorie@comcast.net>
2017-06-15 14:32:39 -0500

    added log in/log out

--- app/controllers/saved_editCtrl.js
+++ app/controllers/saved_editCtrl.js
@@ -1,5 +1,30 @@
 "use strict";
 
-app.controller('Saved_EditCtrl', function(){
+app.controller('Saved_EditCtrl', function($scope, $routeParams, DataFactory, $location) {
+  
+  $scope.task = {
+  	assignedTo: "",
+  	dependencies: "",
+  	dueDate: "",
+  	urgency: "",
+  	description: "",
+  	isCompleted: "",
+  	task: ""
+  };
 
+	DataFactory.getTask($routeParams.taskId)
+	.then( (stuff) => {
+		$scope.task = stuff;
+		$scope.task.id = $routeParams.taskId;
+	});
+
+  $scope.submitTask = function() {
+    // stuff goes here
+    DataFactory.editTask($routeParams.taskId, $scope.task)
+    .then( (response) => {
+    	$location.path("/task-list");
+    });
+    console.log("task", $scope.task);
+    console.log("You clicked the edit task button!");
+  };
 });
