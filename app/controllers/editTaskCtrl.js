ecbd2b7505967dbda7a377fcec20d51ab9424c1b
Ed Sadler <ed_lorie@comcast.net>
2017-06-18 18:06:25 -0500

    made changes to controllers

--- app/controllers/editTaskCtrl.js
+++ app/controllers/editTaskCtrl.js
@@ -1,27 +1,5 @@
 "use strict";
 
-app.controller('EditTaskCtrl', function($scope, DataFactory, $location, AuthFactory) {
 
-let user = AuthFactory.getUser();
 
-  $scope.task = {
-    meal: "",
-    beverage: "",
-    date: "",
-  	isCompleted: false,
-    location: "",
-    task: "",
-    timeOfDay: "",
-    uid: user
-  };
 
-  $scope.submitTask = function () {
-    // stuff goes here
-    console.log("$scope.task", $scope.task);
-    DataFactory.addTask($scope.task)
-    .then ( ( data ) => {
-    	$location.path("/task-list");
-    });
-  };
-
-});
--- app/controllers/taskListCtrl.js
+++ app/controllers/taskListCtrl.js
@@ -1,30 +1,5 @@
 "use strict";
 
-app.controller('TaskListCtrl', function($scope, $routeParams, DataFactory, $location) {
-  
-  $scope.task = {
-  	meal: "",
-  	beverage: "",
-  	date: "",
-  	timeOfDay: "",
-  	description: "",
-  	isCompleted: "",
-  	task: ""
-  };
 
-	DataFactory.getTask($routeParams.taskId)
-	.then( ( stuff ) => {
-		$scope.task = stuff;
-		$scope.task.id = $routeParams.taskId;
-	});
 
-  $scope.submitTask = function() {
-    // stuff goes here
-    DataFactory.editTask($routeParams.taskId, $scope.task)
-    .then( ( response ) => {
-    	$location.path("/task-list");
-    });
-    console.log("task", $scope.task);
-    console.log("You clicked the edit task button!");
-  };
-});
+
